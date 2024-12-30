819b575c599ef85b033b1bafe5be4fedbc26cedf
Sami Abdul-Wahid <syntapy@pm.me>
2024-12-30 15:34:09 -0800

    enclose in <form> not <div>

--- app/src/components/SearchBar/SearchBar.tsx
+++ app/src/components/SearchBar/SearchBar.tsx
@@ -1,11 +1,11 @@
 export default function SearchBar() {
     return (
-        <div>
+        <form>
             <input id="product-filter" type="search" pattern="[a-zA-Z0-9]{3,}" placeholder="Search..." />
             <div>
                 <input id="only-in-stock" type="checkbox" name="only-in-stock" />
                 <label for="only-in-stock">Only show products in stock</label>
             </div>
-        </div>
+        </form>
     )
 }
