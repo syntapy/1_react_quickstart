ab19bac054e4b1ae56aabd62c7510cdf8341c053
Sami Abdul-Wahid <syntapy@pm.me>
2024-12-30 15:18:55 -0800

    add pattern attr

--- app/src/components/SearchBar/SearchBar.tsx
+++ app/src/components/SearchBar/SearchBar.tsx
@@ -1,7 +1,7 @@
 export default function SearchBar() {
     return (
         <div>
-            <input id="product-filter" type="search" placeholder="Search..." />
+            <input id="product-filter" type="search" pattern="[a-zA-Z0-9]{3,}" placeholder="Search..." />
             <div>
                 <input id="only-in-stock" type="checkbox" name="only-in-stock" />
                 <label for="only-in-stock">Only show products in stock</label>
