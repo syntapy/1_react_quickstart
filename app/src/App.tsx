ea9d0c30600219d0401adca859e543524fe4c70f
Sami Abdul-Wahid <syntapy@pm.me>
2025-01-04 21:25:49 -0800

    disable unused hook in App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import './App.css'
 import FilterableProductTable from "./components/FilterableProductTable/index"
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <FilterableProductTable />
