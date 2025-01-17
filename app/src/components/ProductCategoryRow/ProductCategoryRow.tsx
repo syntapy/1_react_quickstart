0edcfeeb72cda2cddd79217f278b6d2945c14a7c
Sami Abdul-Wahid <syntapy@pm.me>
2025-01-17 15:59:45 -0800

    fix minor ProductCategoryRow props error

--- app/src/components/ProductCategoryRow/ProductCategoryRow.tsx
+++ app/src/components/ProductCategoryRow/ProductCategoryRow.tsx
@@ -1,13 +1,15 @@
-interface Prop {
+interface Props {
     name: string
 }
 
 export default function ProductCategoryRow(props: Props) {
     return (
-        <tr className="flex flex-row justify-center">
-            <th colSpan="2">
-                {props.name}
-            </th>
+        <tr 
+          className="flex flex-row justify-center"
+        >
+          <th colSpan="2">
+            {props.name}
+          </th>
         </tr>
     )
 }
