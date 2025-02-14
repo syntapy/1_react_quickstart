fd963a43769c498e64ea3778b903536a6291a67b
Sami Abdul-Wahid <syntapy@pm.me>
2025-02-13 20:01:17 -0800

    fix label for attr in check input component

--- app/src/components/CheckInput/CheckInput.tsx
+++ app/src/components/CheckInput/CheckInput.tsx
@@ -6,7 +6,7 @@ export default function CheckInput(props: Props) {
   return (
     <>
       <input id={props.id_name} type="checkbox" name={props.id_name} />
-      <label htmlFor="only-in-stock">Only show products in stock</label>
+      <label htmlFor={props.id_name}>Only show products in stock</label>
     </>
   )
 }
