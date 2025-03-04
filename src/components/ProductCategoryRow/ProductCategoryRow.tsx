389de28e74c7a966c8a97cd6fa3e77cf2d901495
Sami Abdul-Wahid <syntapy@pm.me>
2025-03-04 15:00:15 -0800

    Style and color tweaks (#20)

--- src/components/ProductCategoryRow/ProductCategoryRow.tsx
+++ src/components/ProductCategoryRow/ProductCategoryRow.tsx
@@ -7,7 +7,7 @@ type Props =  {
 export default function ProductCategoryRow(props: Props) {
   return (
     <tr className={props.className}>
-      <th className="pb-[1px] pt-[2px] md:pb-[2px] md:pt-[4px]" colSpan={2}>
+      <th className="pb-[1px] pt-[5px] md:pb-[2px] md:pt-[7px]" colSpan={2}>
         {props.name}
       </th>
     </tr>
